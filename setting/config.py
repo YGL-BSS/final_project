1701348eeaf8587c5e3409bc0c40b25ca8645771
MarigoldJ <yusang0130@gmail.com>
2021-07-19 03:44:57 +0900

    add main code

--- setting/config.py
+++ setting/config.py
@@ -4,12 +4,30 @@ here = os.path.dirname(__file__)
 
 PATH_BASE = os.path.abspath(os.path.join(here, os.pardir))
 PATH_DATA = os.path.join(PATH_BASE, 'data')
-
-# print(here)
-# print(PATH_BASE)
+PATH_MODEL = os.path.join(PATH_BASE, 'model')
+PATH_LOG = os.path.join(PATH_BASE, 'log')
 
 def get_data_path(data_name):
     '''
     data 폴더의 데이터 파일의 경로를 반환한다.
     '''
     return os.path.join(PATH_DATA, data_name)
+
+def get_model_path(model_name):
+    '''
+    model 폴더의 모델 저장 파일의 경로를 반환한다.
+    '''
+    return os.path.join(PATH_MODEL, model_name)
+
+def get_log_path(log_name):
+    '''
+    log 폴더의 로그 파일 경로 지정
+    '''
+    return os.path.join(PATH_LOG, log_name)
+
+
+if __name__ == '__main__':
+    print('base path :', PATH_BASE)
+    print('base path :', PATH_DATA)
+    print('base path :', PATH_MODEL)
+    print('base path :', PATH_LOG)
