7a231db2c4536c2fbbcfe4a0755e754ff2d0f551
MarigoldJ <yusang0130@gmail.com>
2021-07-20 22:02:10 +0900

    main.py 인자 받는 코드 기본 구조

--- main.py
+++ main.py
@@ -2,3 +2,16 @@
 메인 실행 코드
 '''
 
+import argparse
+import cv2
+
+ap = argparse.ArgumentParser()
+ap.add_argument('-m', '-mode', default='train', help='train / test')
+args = ap.parse_args()
+
+if args.network == 'test':
+    print('Testing model...')
+    # 테스트 코드
+else:
+    print('Training model...')
+    # 학습 코드
