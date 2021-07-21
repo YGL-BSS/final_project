8149141985fda7735befd7218f7296a432457db0
MarigoldJ <yusang0130@gmail.com>
2021-07-21 15:05:29 +0900

    main코드 argparse 항목 추가

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 '''
 메인 실행 코드
+
+실행 할때 아래와 같이 cmd에 입력
+python main.py -m train
+python main.py -m test
 '''
-
 import argparse
-import cv2
 
-ap = argparse.ArgumentParser()
-ap.add_argument('-m', '-mode', default='train', help='train / test')
+import config
+import preprocess
+import modeling
+
+ap = argparse.ArgumentParser(description='Hand Gesture Password 시스템입니다.')
+
+ap.add_argument('-m', '--mode', required='True', help='[ train / test ] 중 선택')
 args = ap.parse_args()
 
-if args.network == 'test':
-    print('Testing model...')
-    # 테스트 코드
-else:
+if args.mode == 'train':
     print('Training model...')
     # 학습 코드
+    
+elif args.mode == 'test':
+    print('Testing model...')
+    # 테스트 코드
