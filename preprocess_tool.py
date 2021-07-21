4a6c05fac379598b1e5157160af9fa61267c3f20
Jenuskii <jenuskii317@gmail.com>
2021-07-21 17:01:50 +0900

    주석 추가(아직 실행불가)

--- preprocess_tool.py
+++ preprocess_tool.py
@@ -2,7 +2,6 @@
 import os
 import numpy as np
 import cv2
-from hand_detect import get_coordinate, make_model
 
 # paths
 now_here = os.path.dirname(__file__)
@@ -15,16 +14,91 @@ cfg_path = os.path.join(model_path,'cross-hands.cfg')
 
 # material functions
 
+def make_model(weights, cfg):
+    net = cv2.dnn.readNet(weights, cfg)
+    layer_names = net.getLayerNames()
+    output_layers = [layer_names[i[0]- 1] for i in net.getUnconnectedOutLayers()]
+    return net, output_layers
+
+def get_coordinate(size, img_path, net, output_layers):
+    '''
+    size in [320, 416, 608]
+    choose 320 for standard
+
+    img_path directly takes image(frame,ndarray)
+    not 'PATH'
+    '''
+    #path를 받는 경우
+    #img = cv2.imread(img_path)
+    
+    #바로 이미지를 받는 경우
+    img = img_path
+    
+    height, width, _ = img.shape 
+
+    blob = cv2.dnn.blobFromImage(img, 0.00392,(size,size), (0,0,0), True, crop=False)
+    net.setInput(blob)
+    outs = net.forward(output_layers)
+
+    # 감지된 좌표 저장
+    boxes = []
+    
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.5:
+                # Hand detect
+                center_x = int(detection[0] * width)
+                center_y = int(detection[1] * height)
+                w = int(detection[2] * width)
+                h = int(detection[3] * height)
+
+                # Rectangle x_start and y_start
+                x = int(center_x - w / 2)
+                y = int(center_y - h / 2)
+
+                boxes.append([x, y, w, h]) 
+    #print(boxes)
+    #print(len(boxes))
+    # if len(boxes) == 1:
+    #     return boxes[0]
+    
+    # else:
+    #     return 'there is no hand or too many hands'
+    if len(boxes):
+        return boxes[0]
+    else:
+        return False
+
+
+
 def dir_item(abs_path):
+    '''
+    매개변수로 특정 디렉토리의 절대경로를 받는다.
+    해당 디렉토리 내의 모든 아이템들의 절대경로를 
+    리스트에 담아 반환한다.
+    '''
     return [os.path.join(abs_path, item) for item in os.listdir(abs_path)]
 
 def mkdir_under_data(dir_name):
+    '''
+    디폴트로 설정된 디렉토리에
+    dir_name을 폴더명으로 갖는
+    하위 디렉토리를 생성한다.
+    '''
     abs_path = os.path.join(data_path, dir_name)
     try:
         if not os.path.exists(abs_path): os.makedirs(abs_path)
     except OSError: print('can not create dir')
 
 def resize_hand(frame, hand_area, height, width, alpha=0.1):
+    '''
+    frame 에서 [x,y,w,h]값을 참조,
+    alpha 값만큼 늘리거나 줄인 영역을 슬라이싱.
+    height,widht 사이즈로 리사이즈하여 반환한다.
+    '''
     x, y, w, h = hand_area
     y_start = round(y * (1-alpha))
     x_start = round(x * (1-alpha))
@@ -34,11 +108,24 @@ def resize_hand(frame, hand_area, height, width, alpha=0.1):
     return cv2.resize(target, (width, height), interpolation=cv2.INTER_CUBIC)
 
 def write_img(video_name,currentframe,resized_frame):
+    '''
+    설정된 경로에서 비디오의 앞 3글자 디렉토리에
+    비디오명 끝 3자리(.확장자명 제외)를 앞 이름을 갖는
+    이미지를 저장한다.
+    '''
     folder = os.path.join(data_path, video_name[-11:-8])
     name = f'{video_name[-7:-4]}_{currentframe:0>5}.jpg'
     cv2.imwrite(os.path.join(folder,name), resized_frame)
 
 def onehot_ndarray(y_cnt_lst):
+    '''
+    예시) [333,222,550]이라는 리스트를 매개변수로 전달할 경우
+    위에서부터 차례로
+    [1 0 0] 이 333개
+    [0 1 0] 이 222개
+    [0 0 1] 이 550개
+    append된 2차원 ndarray를 반환. 이경우 shape는 (1105,3)
+    '''
     Y = np.empty((0,len(y_cnt_lst)),dtype=int)
     for i in range(len(y_cnt_lst)):
             temp = [0] * len(y_cnt_lst)
@@ -51,6 +138,18 @@ def onehot_ndarray(y_cnt_lst):
 # main function
 
 def videos_to_data(height=224, width=224, channel=3, get_npy=True, get_images=False):
+    '''
+    get_npy=True 기본값
+    기본적으로 (X_data, Y_data)를 반환. 각각 ndarray
+    그와 동시에 npy 파일을 저장
+
+    get_images=False
+    True로 설정 시 이미지를 각label 디렉토리에 저장
+    height, width 기본값 224
+    '''
+    
+    
+    
     #model set
     net, output_layers = make_model(weights_path,cfg_path)
 
