14ad65827dd7e29cae7be57a73b5e77615515486
MarigoldJ <yusang0130@gmail.com>
2021-07-21 16:53:59 +0900

    main 코드에 ready 옵션 추가

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ python main.py -m train
 python main.py -m test
 '''
 import argparse
+import wget
+import os
 
 import config
 import preprocess
@@ -13,13 +15,37 @@ import modeling
 
 ap = argparse.ArgumentParser(description='Hand Gesture Password 시스템입니다.')
 
-ap.add_argument('-m', '--mode', required='True', help='[ train / test ] 중 선택')
+ap.add_argument('-m', '--mode', required='True', default='', help='[ ready / train / test ] 중 선택')
 args = ap.parse_args()
 
-if args.mode == 'train':
+if args.mode == 'ready':
+    print('Ready for Survice...')
+    
+    # Yolo pretrained model 불러오기
+    if not os.path.isdir('./yolo'):
+        os.mkdir('./yolo')
+    
+    url_list = [
+        'https://github.com/cansik/yolo-hand-detection/releases/download/pretrained/cross-hands.cfg',
+        'https://github.com/cansik/yolo-hand-detection/releases/download/pretrained/cross-hands.weights',
+
+        'https://github.com/cansik/yolo-hand-detection/releases/download/pretrained/cross-hands-tiny-prn.cfg',
+        'https://github.com/cansik/yolo-hand-detection/releases/download/pretrained/cross-hands-tiny-prn.weights',
+        
+        'https://github.com/cansik/yolo-hand-detection/releases/download/pretrained/cross-hands-yolov4-tiny.cfg',
+        'https://github.com/cansik/yolo-hand-detection/releases/download/pretrained/cross-hands-yolov4-tiny.weights'
+    ]
+
+    for url in url_list:
+        wget.download(url, out='./yolo')
+
+elif args.mode == 'train':
     print('Training model...')
     # 학습 코드
     
 elif args.mode == 'test':
     print('Testing model...')
     # 테스트 코드
+
+else:
+    print('')
